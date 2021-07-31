78f6bacd09f349ce41ea0e09e10198183414340c
Suvash Thapaliya <suvash@gmail.com>
2021-07-31 03:25:42 +0200

    Enable docker nvidia support (with cgroups v1)

--- hosts/prayogshala/configuration.nix
+++ hosts/prayogshala/configuration.nix
@@ -168,6 +168,12 @@
   virtualisation.docker.enable = true;
   virtualisation.docker.enableOnBoot = true;
 
+  # Nvidia Docker
+  virtualisation.docker.enableNvidia = true;
+  # libnvidia-container does not support cgroups v2
+  # https://github.com/NVIDIA/nvidia-docker/issues/1447
+  systemd.enableUnifiedCgroupHierarchy = false;
+
   # Podman
   virtualisation.podman.enable = false;
 
--- hosts/prayogshala/intel_cpu.nix
+++ hosts/prayogshala/intel_cpu.nix
@@ -14,6 +14,7 @@
       vaapiVdpau
       libvdpau-va-gl
     ];
+    driSupport32Bit = true;
   };
 
 }
