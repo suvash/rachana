9638372777becc04bc4dfcb05d476c9449aaa0c8
Suvash Thapaliya <suvash@gmail.com>
2022-12-09 15:30:54 +0100

    Use chemacs with doom vanilla & legacy profiles

---
+++ common/config/chemacs/emacs-profiles.el
@@ -0,0 +1,3 @@
+(("doom" . ((user-emacs-directory . "~/.config/doom-emacs.d")))
+ ("legacy" . ((user-emacs-directory . "~/.config/legacy-emacs.d")))
+ ("vanilla"  . ((user-emacs-directory . "~/.config/vanilla-emacs.d"))))
--- hosts/karkhana/provision/init.sh
+++ hosts/karkhana/provision/init.sh
@@ -10,11 +10,6 @@ if [ ! -L /etc/nixos/configuration.nix ]; then
   sudo ln -sfnv "$RACHANA/hosts/$HOSTNAME/hardware-configuration.nix" /etc/nixos/hardware-configuration.nix
 fi
 
-if [ ! -L "$HOME/.emacs.d" ]; then
- mv "$HOME/.emacs.d" "$HOME/.emacs.d.orig"
- ln -sfnv "$RACHANA/common/config/.emacs.d" "$HOME/.emacs.d"
-fi
-
 if [ ! -L "$HOME/.config/git" ]; then
  mv "$HOME/.config/git" "$HOME/.config/git.orig"
  ln -sfnv "$RACHANA/common/config/git" "$HOME/.config/git"
@@ -24,3 +19,24 @@ if [ ! -d "$HOME/.config/base16-shell" ]; then
   git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 fi
 
+
+if [ ! -d "$HOME/.emacs.d" ]; then
+  git clone https://github.com/plexus/chemacs2.git ~/.emacs.d
+  ln -sfnv "$RACHANA/common/config/chemacs/emacs-profiles.el" "$HOME/.emacs-profiles.el"
+  ln -sfnv "$RACHANA/common/config/chemacs/emacs-profile" "$HOME/.emacs-profile"
+fi
+
+if [ ! -L "$HOME/.config/legacy-emacs.d" ]; then
+  rm -rf "$HOME/.config/legacy-emacs.d"
+  ln -sfnv "$RACHANA/common/config/.emacs.d" "$HOME/.config/legacy-emacs.d"
+fi
+
+if [ ! -d "$HOME/.config/doom-emacs.d" ]; then
+  git clone https://github.com/hlissner/doom-emacs ~/.config/doom-emacs.d
+  ~/.config/doom-emacs.d/bin/doom install
+  ~/.config/doom-emacs.d/bin/doom doctor
+fi
+
+if [ ! -d "$HOME/.config/vanilla-emacs.d" ]; then
+  mkdir ~/.config/vanilla-emacs.d
+fi
