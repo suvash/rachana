4a4e62448b6318a33f5b751e9148aee16c8ae5df
Suvash Thapaliya <suvash@gmail.com>
2023-02-11 16:19:50 +0100

    chemacs2, doom, legacy emacs and nvim

--- hosts/nepathya/provision/init.sh
+++ hosts/nepathya/provision/init.sh
@@ -10,12 +10,6 @@ if [ ! -L "$HOME/.nixpkgs/darwin-configuration.nix" ]; then
   ln -sfnv "$HOME/Developer/rachana/hosts/nepathya/configuration.nix" "$HOME/.nixpkgs/darwin-configuration.nix"
 fi
 
-echo "-- Setting up emacs configuration symlink"
-if [ ! -L "$HOME/.emacs.d" ]; then
-  mv "$HOME/.emacs.d" "$HOME/.emacs.d.orig"
-  ln -sfnv "$HOME/Developer/rachana/common/config/.emacs.d" "$HOME/.emacs.d"
-fi
-
 echo "-- Setting up git configuration symlink"
 if [ ! -L "$HOME/.config/git" ]; then
   mv "$HOME/.config/git" "$HOME/.config/git.orig"
@@ -40,4 +34,30 @@ echo "-- Cloning down base16-shell symlink"
 if [ ! -d "$HOME/.config/base16-shell" ]; then
   mkdir -p "$HOME/.config"
   git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+fi
+
+echo "-- Cloning down chemacs2"
+if [ ! -d "$HOME/.emacs.d" ]; then
+  git clone https://github.com/plexus/chemacs2.git ~/.emacs.d
+fi
+
+echo "-- Setting up chemacs2 profiles"
+if [ ! -L "$HOME/.emacs-profiles.el" ]; then
+  ln -sfnv "$HOME/Developer/rachana/common/config/emacs/.emacs-profiles.el" "$HOME/.emacs-profiles.el"
+fi
+
+echo "-- Cloning down doom-emacs"
+if [ ! -d "$HOME/.doom-emacs.d" ]; then
+  git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.doom-emacs.d
+  $HOME/.doom-emacs.d/bin/doom install
+fi
+
+echo "-- Setting up legacy emacs configuration symlink"
+if [ ! -L "$HOME/.legacy-emacs.d" ]; then
+  ln -sfnv "$HOME/Developer/rachana/common/config/.emacs.d" "$HOME/.legacy-emacs.d"
+fi
+
+echo "-- Cloning down packer.nvim to packpath"
+if [ ! -d "$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim" ]; then
+  git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 fi
