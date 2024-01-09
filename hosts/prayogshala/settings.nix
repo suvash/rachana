d62dac76ea84365608912c4db714ee6d9430f4b9
Suvash Thapaliya <suvash@gmail.com>
2024-01-09 02:39:58 +0100

    back to kitty

--- hosts/prayogshala/settings.nix
+++ hosts/prayogshala/settings.nix
@@ -24,7 +24,7 @@
         type = with lib.types; uniq str;
       };
       terminal = lib.mkOption {
-        default = "cool-retro-term";
+        default = "kitty";
         type = with lib.types; uniq str;
       };
       editor = lib.mkOption {
